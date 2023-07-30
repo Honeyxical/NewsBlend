8a2f15a8905a63e77579aa374da00693221bb550
Ilya Benikov <ilya.benikov@gmail.com>
2023-07-30 15:58:02 +0400

    Initial Commit

---
+++ NewsBlend/ViewController.swift
@@ -0,0 +1,11 @@
+//  Created by илья on 30.07.23.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
