658e8139dad26628be057242710977ecc29aee10
Ilya Benikov <ilya.benikov@gmail.com>
2023-08-01 22:56:53 +0400

    Create module for settngs

---
+++ NewsBlend/SettingModule/Router/SettingsRouter.swift
@@ -0,0 +1,11 @@
+//  Created by илья on 01.08.23.
+
+import Foundation
+
+class SettingsRouter {
+
+}
+
+extension SettingsRouter: SettingsRouterInputProtocol {
+    
+}
---
+++ NewsBlend/SettingModule/Services/CoreData/SettingCoreDataService.swift
@@ -0,0 +1,7 @@
+//  Created by илья on 01.08.23.
+
+import Foundation
+
+final class SettingsCoreDataService: SettingsCoreDataServiceProtocol {
+    
+}
