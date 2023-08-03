b01bf123e4113effb9cc3a38e1b1b3cbbdf0f530
Ilya Benikov <ilya.benikov@gmail.com>
2023-08-03 14:15:25 +0400

    Create UIColor extension

---
+++ NewsBlend/Extension/Color + Extension.swift
@@ -0,0 +1,8 @@
+//  Created by илья on 03.08.23.
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let lightBlue = UIColor(red: 230 / 255, green: 237 / 255, blue: 236 / 255, alpha: 1)
+}
--- NewsBlend/FeedModule/Contracts/FeedContracts.swift
+++ NewsBlend/FeedModule/Contracts/FeedContracts.swift
@@ -13,24 +13,29 @@ public protocol FeedModuleOutputProtocol {
 // View Input
 protocol FeedViewInputProtocol {
     func set(articles: [Articles])
+    func set(hot articles: [Articles])
     func reloadData()
     func showLoader()
     func hideLoader()
+    func displayLotty()
 }
 
 // View Output
 protocol FeedViewOutputProtocol {
     func loadData()
+    func loadHotData()
 }
 
 // Interactor Input
 protocol FeedInteractorInputProtocol {
     func loadData()
+    func loadHotData()
 }
 
 // Interactor Output
 protocol FeedInteractorOutputProtocol: AnyObject {
     func didReceive(articles: [Articles])
+    func didReceive(hot articles: [Articles])
     func didReceiveFail()
 }
 
@@ -43,7 +48,8 @@ protocol FeedRouterOutputProtocol {
 }
 
 protocol FeedNetworkServiceProtocol {
-    func getNewsFromNewsApi(completiton: @escaping(Data) -> Void)
+    func getNews(completiton: @escaping(Data) -> Void)
+    func getHotNews(country: String, completiton: @escaping(Data) -> Void)
 }
 
 protocol FeedCoreDataServiceProtocol{}
