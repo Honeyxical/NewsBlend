4e24e038bf139ae818688693a2d3e2b6112177f3
Ilya Benikov <ilya.benikov@gmail.com>
2023-09-08 13:25:46 +0400

    rewrote classes to structures

--- NewsBlend/Modules/FeedModule/Interactor/ArticleModel.swift
+++ NewsBlend/Modules/FeedModule/Interactor/ArticleModel.swift
@@ -2,7 +2,7 @@
 
 import Foundation
 
-class ArticleModel: NSObject, NSCoding {
+struct ArticleModel: Equatable, Encodable, Decodable {
     let source: SourceModel
     let author: String
     let title: String
@@ -11,51 +11,4 @@ class ArticleModel: NSObject, NSCoding {
     let publishedAt: String
     var timeSincePublication: String // Время с момента публикации
     let content: String
-    
-    init(source: SourceModel, author: String, title: String, desctiption: String, urlToImage: String, publishedAt: String, timeSincePublication: String, content: String) {
-        self.source = source
-        self.author = author
-        self.title = title
-        self.desctiption = desctiption
-        self.urlToImage = urlToImage
-        self.publishedAt = publishedAt
-        self.timeSincePublication = timeSincePublication
-        self.content = content
-        super.init()
-    }
-    
-    func encode(with coder: NSCoder) {
-        coder.encode(source, forKey: "source")
-        coder.encode(author, forKey: "author")
-        coder.encode(title, forKey: "title")
-        coder.encode(desctiption, forKey: "desctiption")
-        coder.encode(urlToImage, forKey: "urlToImage")
-        coder.encode(publishedAt, forKey: "publishedAt")
-        coder.encode(timeSincePublication, forKey: "timeSincePublication")
-        coder.encode(content, forKey: "content")
-        
-    }
-    
-    required init?(coder: NSCoder) {
-        self.source = coder.decodeObject(forKey: "source") as? SourceModel ?? SourceModel(id: "", name: "", category: "", language: "", country: "", isSelected: false)
-        self.author = coder.decodeObject(forKey: "author") as? String ?? ""
-        self.title = coder.decodeObject(forKey: "title") as? String ?? ""
-        self.desctiption = coder.decodeObject(forKey: "desctiption") as? String ?? ""
-        self.urlToImage = coder.decodeObject(forKey: "urlToImage") as? String ?? ""
-        self.publishedAt = coder.decodeObject(forKey: "publishedAt") as? String ?? ""
-        self.timeSincePublication = coder.decodeObject(forKey: "timeSincePublication") as? String ?? ""
-        self.content = coder.decodeObject(forKey: "content") as? String ?? ""
-        super.init()
-    }
-    
-    override func isEqual(_ object: Any?) -> Bool {
-        if let otherArticle = object as? ArticleModel {
-            return source == otherArticle.source &&
-            author == otherArticle.author &&
-            title == otherArticle.title &&
-            desctiption == otherArticle.desctiption &&
-            content == otherArticle.content
-        }
-        return false
-    }
 }
--- NewsBlend/Modules/SettingModule/Interactor/SourceModel.swift
+++ NewsBlend/Modules/SettingModule/Interactor/SourceModel.swift
@@ -2,53 +2,11 @@
 
 import Foundation
 
-class SourceModel: NSObject {
+struct SourceModel: Equatable, Encodable, Decodable {
     var id: String
     var name: String
     var category: String
     var language: String
     var country: String
     var isSelected: Bool
-
-    init(id: String,
-         name: String,
-         category: String,
-         language: String,
-         country: String,
-         isSelected: Bool) {
-        self.id = id
-        self.name = name
-        self.category = category
-        self.language = language
-        self.country = country
-        self.isSelected = isSelected
-    }
-
-    required init?(coder: NSCoder) {
-        self.id = coder.decodeObject(forKey: "id") as? String ?? ""
-        self.name = coder.decodeObject(forKey: "name") as? String ?? ""
-        self.category = coder.decodeObject(forKey: "category") as? String ?? ""
-        self.language = coder.decodeObject(forKey: "language") as? String ?? ""
-        self.country = coder.decodeObject(forKey: "country") as? String ?? ""
-        self.isSelected = coder.decodeBool(forKey: "isSelected")
-    }
-    
-    override func isEqual(_ object: Any?) -> Bool {
-        if let otherSource = object as? SourceModel {
-            return id == otherSource.id &&
-                   name == otherSource.name
-        }
-        return false
-    }
-}
-
-extension SourceModel: NSCoding {
-    func encode(with coder: NSCoder) {
-        coder.encode(id, forKey: "id")
-        coder.encode(name, forKey: "name")
-        coder.encode(category, forKey: "category")
-        coder.encode(language, forKey: "language")
-        coder.encode(country, forKey: "country")
-        coder.encode(isSelected, forKey: "isSelected")
-    }
 }
