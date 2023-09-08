7a1f08f3bc7c77dd4a12f8d605aa13deffb5050e
Ilya Benikov <ilya.benikov@gmail.com>
2023-09-08 16:29:48 +0400

    Fix parser

--- NewsBlend/Helpers/Parser.swift
+++ NewsBlend/Helpers/Parser.swift
@@ -17,17 +17,13 @@ class Parser: ParserProtocol {
         for source in sources {
             group.enter()
             networkService.getArticlesBySource(source: source, pageSize: pageSize) { data in
-                if data.isEmpty {
-                    group.leave()
-                    return
-                }
                 guard let articlesDTO = try? JSONDecoder().decode(NewsModelDTO.self, from: data) else { return }
                 articles += Converter.setDate(articles: Converter.transferDTOtoModel(articlesArray: articlesDTO.articles))
                 group.leave()
+            } failure: {
+                group.leave()
+                return
             }
-//        }failure: {
-//
-//            }
         }
         
         group.notify(queue: DispatchQueue.main) {
@@ -39,52 +35,33 @@ class Parser: ParserProtocol {
     func parseFeedSource(source: SourceModel, articlesCount: Int, network: FeedNetworkServiceProtocol, completion: @escaping ([ArticleModel]) -> Void) {
         network.getArticles(source: source, articlesCount: articlesCount){ data in
             var articles: [ArticleModel] = []
-            if data.isEmpty {
-                completion(articles)
-            }
             guard let articlesDTO = try? JSONDecoder().decode(NewsModelDTO.self, from: data) else { return }
             articles = Converter.transferDTOtoModel(articlesArray: articlesDTO.articles)
             articles = articles.sorted { $0.publishedAt > $1.publishedAt }
             completion(articles)
+        } failure: {
+            completion([])
         }
     }
     
     func parseNBSArticlesBySource(source: SourceModel, pageSize: Int, network: NBSNetworkServiceProtocol, completion: @escaping ([ArticleModel]) -> Void) {
         var articles: [ArticleModel] = []
-        let group = DispatchGroup()
-        
-        group.enter()
+
         network.getArticlesBySource(source: source, pageSize: pageSize) { data in
-            if data.isEmpty {
-                group.leave()
-            }
             guard let articlesDTO = try? JSONDecoder().decode(NewsModelDTO.self, from: data) else { return }
             articles = Converter.transferDTOtoModel(articlesArray: articlesDTO.articles)
-            group.leave()
-        }
-        
-        group.notify(queue: DispatchQueue.main) {
-            articles = articles.sorted { $0.publishedAt > $1.publishedAt }
+        } failure: {
             completion(articles)
         }
     }
     
     func parseSource(defaultLanguage: String, network: SettingNetworkServiceProtocol, completion: @escaping ([SourceModel]) -> Void) {
         var sources: [SourceModel] = []
-        let group = DispatchGroup()
-        
-        group.enter()
+
         network.getSources(sourceLanguage: defaultLanguage) { data in
-            if data.isEmpty {
-                group.leave()
-            }
             guard let sourcesDTO = try? JSONDecoder().decode(SourcesModelDTO.self, from: data) else { return }
             sources = Converter.transferSourceObject(sources: sourcesDTO.sources)
-            group.leave()
-        }
-        
-        group.notify(queue: DispatchQueue.main) {
-            sources = sources.sorted { $0.name < $1.name }
+        } failure: {
             completion(sources)
         }
     }
